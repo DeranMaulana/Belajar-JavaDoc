7db0e64948a26c7604bcb91babbc7c10baeb18f1
DeranMaulana <123473573+DeranMaulana@users.noreply.github.com>
2024-11-07 20:32:04 +0700

    Add files via upload

---
+++ Belajar JavaDoc/src/main/java/JavaDoc/TesJava.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package JavaDoc;
+
+/**
+ *
+ * @author deran
+ */
+public class TesJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
